671a5ed2a1616dc144180e566215dda9dcd79095
Timirkumarpatel <timir.patel@iwantunlimited.com>
2021-06-07 17:49:01 +0530

    Added GenericObject for Feign Data Exchange

---
+++ src/main/java/io/itpl/microservice/common/GenericObject.java
@@ -0,0 +1,52 @@
+package io.itpl.microservice.common;
+
+import io.itpl.microservice.base.BaseObject;
+
+public class GenericObject extends BaseObject  {
+
+    private String id;
+    private String name;
+    private String title;
+    private String userId;
+    private UserObject user;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public UserObject getUser() {
+        return user;
+    }
+
+    public void setUser(UserObject user) {
+        this.user = user;
+    }
+}
