109ba62d6a9d44eb407dacd382dc767a0ead6f0b
Mayur Deshmukh <mayur.deshmukh@iwantunlimited.com>
2021-07-03 13:17:58 +0530

    Reaction Added for Emojis

--- src/main/java/io/itpl/microservice/common/Reaction.java
+++ src/main/java/io/itpl/microservice/common/Reaction.java
@@ -3,16 +3,20 @@ package io.itpl.microservice.common;
 public class Reaction {
 	
 	 private String id;
-	 private String title;
 	 private String userId;
 	 private String reaction;
+	 private UserObject userObject;
+	 private String createdOn;
 	 
 	public String getId() {	return id;}
 	public void setId(String id) {this.id = id;}
-	public String getTitle() {return title;}
-	public void setTitle(String title) {this.title = title;}
 	public String getUserId() {return userId;}
 	public void setUserId(String userId) {this.userId = userId;}
 	public String getReaction() {return reaction;}
-	public void setReaction(String reaction) {this.reaction = reaction;}	 	 
+	public void setReaction(String reaction) {this.reaction = reaction;}
+	public UserObject getUserObject() {return userObject;}
+	public void setUserObject(UserObject userObject) {this.userObject = userObject;}
+	public String getCreatedOn() {return createdOn;}
+	public void setCreatedOn(String createdOn) {this.createdOn = createdOn;}	
+	
 }
