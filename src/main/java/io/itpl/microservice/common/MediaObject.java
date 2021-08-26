4f0106a4be5bac20344c45dd9f8c15454473fb5c
MayurDes <63035192+MayurDes@users.noreply.github.com>
2021-08-26 14:21:22 +0530

    Reverted Commit

--- src/main/java/io/itpl/microservice/common/MediaObject.java
+++ src/main/java/io/itpl/microservice/common/MediaObject.java
@@ -87,19 +87,6 @@ public class MediaObject {
 	 */
 	private List<String>mediaSource;
 	
-	/**
-	* Type - Other, 
-	*/
-	private String jsonObject;
-
-	public String getJsonObject() {
-            return jsonObject;
-        }
-
-	public void setJsonObject(String jsonObject) {
-	    this.jsonObject = jsonObject;
-	}
-
 	public List<String> getMediaSource() {
 		return mediaSource;
 	}
