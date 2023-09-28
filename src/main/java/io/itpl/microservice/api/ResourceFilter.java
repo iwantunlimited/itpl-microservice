10f08a56562f77bb377de64a6635a7a37e64d0b2
malav <malav.mevada@itpl.io>
2023-09-28 11:06:26 +0530

    Added some attributes in ResourceFilter

--- src/main/java/io/itpl/microservice/api/ResourceFilter.java
+++ src/main/java/io/itpl/microservice/api/ResourceFilter.java
@@ -1,66 +1,80 @@
 package io.itpl.microservice.api;
 
 public class ResourceFilter {
-	private int width;
-	private int height;
-	public ResourceFilter() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public ResourceFilter(int width, int height) {
-		super();
-		this.width = width;
-		this.height = height;
-	}
+    private int width;
+    private int height;
+    private boolean thumbnail;
 
-	public static ResourceFilter from(int width,int height) {
-		return new ResourceFilter(width,height);
-	}
+    private boolean original;
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + height;
-		result = prime * result + width;
-		return result;
-	}
+    public ResourceFilter() {
+        // TODO Auto-generated constructor stub
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ResourceFilter other = (ResourceFilter) obj;
-		if (height != other.height)
-			return false;
-		if (width != other.width)
-			return false;
-		return true;
-	}
+    public ResourceFilter(int width, int height) {
+        super();
+        this.width = width;
+        this.height = height;
+    }
 
-	public int getWidth() {
-		return width;
-	}
+    public static ResourceFilter from(int width, int height) {
+        return new ResourceFilter(width, height);
+    }
 
-	public void setWidth(int width) {
-		this.width = width;
-	}
+    public boolean isOriginal() {
+        return original;
+    }
 
-	public int getHeight() {
-		return height;
-	}
+    public void setOriginal(boolean original) {
+        this.original = original;
+    }
 
-	public void setHeight(int height) {
-		this.height = height;
-	}
+    public boolean isThumbnail() {
+        return thumbnail;
+    }
 
-	@Override
-	public String toString() {
-		return "ResourceFilter [width=" + width + ", height=" + height + "]";
-	}
+    public void setThumbnail(boolean thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + height;
+        result = prime * result + width;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        ResourceFilter other = (ResourceFilter) obj;
+        if (height != other.height) return false;
+        return width == other.width;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "ResourceFilter [width=" + width + ", height=" + height + "]";
+    }
 
 }
